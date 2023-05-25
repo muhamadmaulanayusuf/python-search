533af78a52127d60ed018d08c804e37c8d6f6aa7
muhamadmaulanayusuf <134568471+muhamadmaulanayusuf@users.noreply.github.com>
2023-05-25 14:18:02 +0700

    Add files via upload

---
+++ sequential_search_library.py
@@ -0,0 +1,26 @@
+def is_similar(title1, title2):
+    title1 = title1.lower()
+    title2 = title2.lower()
+
+    if title1[0] == title2[0]:
+        return True
+    
+    return False
+
+def sequential_search(books, book_title):
+    for book in books:
+        if is_similar(book['title1'], book_title):
+            return book['shelf']
+        
+    return "Buku tidak ditemukan"
+
+books = [
+    {'title': 'Python Programming', 'shelf': 'A1'},
+    {'title': 'Data Structure and Algorithms', 'shelf': 'B2'},
+    {'title': 'Introduction to Machine Learning', 'shelf': 'C3'},
+    {'title': 'Database Management Systems', 'shelf': 'D4'},
+]
+
+book_title = input("Masukan judul buku yang ingin dicari: ")
+shelf_location = sequential_search(books, book_title)
+print(shelf_location)
